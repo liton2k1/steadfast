139df1dd22e2cb9b1d2be0c2c8ba305e0e281f57
liton61 <litonofficial61@gmail.com>
2025-07-01 03:53:58 +0600

    base url updated

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "http://157.230.240.97:9999/api/v1",
+ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || "http://157.230.240.97:9999/api/v1",
 });
 
 
